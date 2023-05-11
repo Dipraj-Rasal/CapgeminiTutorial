493eb5d1ae6d6d82a32e06107829eaf7c9875f67
Dipraj-Rasal <132876694+Dipraj-Rasal@users.noreply.github.com>
2023-05-11 15:05:43 +0530

    Add files via upload

---
+++ Dipraj_Rasal_dowhileloop.ps1
@@ -0,0 +1,82 @@
+﻿$c=0
+do{
+ $filename='Dipraj'+$c+'.txt';
+ new-item -path $filename -itemtype file 
+ $c++
+
+}while($c -lt 10)
+
+
+$c=0
+do{
+ $filename='Dipraj'+$c;
+ new-item -path $filename -itemtype directory 
+ $c++
+
+}while($c -lt 10)
+
+
+$c=0
+do{ 
+    $fdr='Dipraj'+$c;
+    $d=0
+    do{
+        $fdr1='Dip'+$d;
+        new-item -path $fdr\$fdr1 -itemtype directory 
+        $d++
+      }while($d -lt 10)
+    $c++
+  }while($c -lt 10)
+
+
+$c=0
+do{ 
+    $fdr='Dipraj'+$c;
+    $d=0
+    do{
+        $fdr1='Dip'+$d;
+        $e=0
+        do{
+        $fdr2='Dip'+$e+'.txt'
+        new-item -path $fdr\$fdr1\$fdr2 -itemtype file
+        $e++
+        }while($e -lt 10)
+         
+        $d++
+      }while($d -lt 10)
+    $c++
+  }while($c -lt 10)
+
+
+  $c=0
+do{ 
+    $fdr='Dipraj'+$c;
+    $d=0
+    do{
+        $fdr1='Dip'+$d;
+        $e=0
+        do{
+        $file='Dipraj'+$e+'.txt'
+        copy-item -path $file -recurse -destination $fdr\$fdr1
+        $e++
+        }while($e -lt 10)
+         
+        $d++
+      }while($d -lt 10)
+    $c++
+  }while($c -lt 10)
+
+
+
+  $c=0
+do{ 
+    $fdr='Dipraj'+$c;
+    $d=0
+    do{
+        $fdr1='Dip'+$d;
+        remove-item $fdr\$fdr1\* 
+        $d++
+      }while($d -lt 10)
+    $c++
+  }while($c -lt 10)
+
---
+++ Dipraj_Rasal_whileloop.ps1
@@ -0,0 +1,81 @@
+﻿$c=1
+while($c -lt 11){
+$filename='Dipraj'+$c+'.txt';
+ new-item -path $filename -itemtype file 
+ $c++
+}
+
+$c=1
+while($c -lt 11){
+$filename='Dipraj'+$c;
+ new-item -path $filename -itemtype directory 
+ $c++
+}
+
+
+$c=1
+while($c -lt 11){
+    $fdr='Dipraj'+$c;
+    $d=1
+    while($d -lt 11){
+    $fdr1='Dip'+$d;
+    new-item -path $fdr\$fdr1 -itemtype directory
+    $d++
+    }
+    $c++
+}
+
+
+$c=1
+while($c -lt 11){
+    $fdr='Dipraj'+$c;
+    $d=1
+    while($d -lt 11){
+    $fdr1='Dip'+$d;
+    $e=1
+    while($e -lt 11){
+    
+        $fdr2='D'+$e+'.txt';
+        new-item -path $fdr\$fdr1\$fdr2 -itemtype file
+        $e++
+        }        
+        $d++
+    }
+    $c++
+}
+
+
+$c=1
+while($c -lt 11){
+    $fdr='Dipraj'+$c;
+    $d=1
+    while($d -lt 11){
+    $fdr1='Dip'+$d;
+    $e=1
+    while($e -lt 11){
+    
+        $file='Dipraj'+$e+'.txt';
+        copy-item -path $file -recurse -destination $fdr\$fdr1
+        $e++
+        }        
+        $d++
+    }
+    $c++
+}
+
+
+$c=1
+while($c -lt 11){
+    $fdr='Dipraj'+$c;
+    $d=1
+    while($d -lt 11){
+    $fdr1='Dip'+$d;
+    remove-item $fdr\$fdr1\* -recurse
+    $d++
+    }
+    $c++
+}
+
+
+
+
